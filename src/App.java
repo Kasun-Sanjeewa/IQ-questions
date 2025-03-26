757e8b52bd16a11a54da485b8dc07ccfc94b904a
Kasun3865 <ict21945@fot.sjp.ac.lk>
2025-03-27 01:10:46 +0530

    add palindrome

--- src/App.java
+++ src/App.java
@@ -1,12 +1,35 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String str = "Hello";
+        String str = "repaper";
         String reverseStr = "";
+        int count = 0;
 
         for (int i = 0; i < str.length(); i++) {
             reverseStr = str.charAt(i) + reverseStr;
         }
 
-        System.out.println(reverseStr);
+        for (int j = 0; j < str.length(); j++) {
+            if (str.charAt(j) == reverseStr.charAt(j)) {
+                count = count + 1;
+            } else {
+                System.out.println("THis is not Palindrome");
+                break;
+            }
+
+            if (str.length() == count) {
+                System.out.println("THis is Palindrome");
+            }
+        }
+        // ----------------------------------------------
+        // for (int i = 0; i < str.length(); i++) {
+        // reverseStr = str.charAt(i) + reverseStr;
+        // }
+
+        // if (str.equals(reverseStr)) {
+        // System.out.println("THis is Palindrome");
+        // } else {
+        // System.out.println("THis is not Palindrome");
+        // }
+
     }
 }
