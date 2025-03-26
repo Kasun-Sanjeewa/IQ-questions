a5b98553829a108c1668bf7409fa732763228d1b
Kasun3865 <ict21945@fot.sjp.ac.lk>
2025-03-27 00:40:41 +0530

    reverse a string

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String str = "Hello";
+        String reverseStr = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            reverseStr = str.charAt(i) + reverseStr;
+        }
+
+        System.out.println(reverseStr);
     }
 }
